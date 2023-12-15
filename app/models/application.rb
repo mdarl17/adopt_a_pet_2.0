cd54bf565449d26fb3e7bd28213df6e515808154
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-14 23:11:45 -0700

    To implement pet application pet updates I redefined #all_pets_apps_appr? which returned a boolean, to #check_app_status, which is more descriptive of what it does, and it also allows the pet application show page to have a 'Pending' state. Otherwise, the application would fail unless and until all pets were Approved.

--- app/models/application.rb
+++ app/models/application.rb
@@ -10,17 +10,30 @@ class Application < ApplicationRecord
     Pet.where("name = ?", name)
   end
 
-  def get_pet_app(pet_id) 
+  def get_pet_app(pet_id)
     PetApplication.where(pet_id: pet_id, application_id: self.id).first
   end
 
-  def all_pets_apps_appr?
-    if self.pet_applications.where('status = 2').count == self.pet_applications.count &&
-      self.pet_applications.where('status = 2').count > 0
-        true
-    elsif self.pet_applications.where('status = 3').count > 0 &&
-       (self.pet_applications.count - self.pet_applications.where('status = 3').count) == self.pet_applications.where('status = 2').count
-        false
+  def check_app_status
+    rejected = pet_applications.where("pet_applications.status = 2").any?
+    pending = pet_applications.where("pet_applications.status = 0").any?
+
+    if rejected
+      "Rejected"
+    elsif pending
+      "Pending"
+    else
+      "Approved"
     end
   end
+
+  # def all_pets_apps_appr?
+  #   if self.pet_applications.where('status = 1').count == self.pet_applications.count &&
+  #     self.pet_applications.where('status = 1').count > 0 && !self.pet_applications.where("status = 0").count > 0
+  #       true
+  #   elsif self.pet_applications.where('status = 2').count > 0 &&
+  #      (self.pet_applications.count - self.pet_applications.where('status = 2').count) == self.pet_applications.where('status = 1').count
+  #       false
+  #   end
+  # end
 end
