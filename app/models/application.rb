dcd3dffb06a690adf26b4e2ffb876d273c62e68b
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-18 13:29:46 -0700

    Add three new boolean instance methods - approved?, rejected?, pending? - to handle calls from the pet application controller's 'update' method needing to know how it's most recent update to a pet application affects the overall, general application's status.

--- app/models/application.rb
+++ app/models/application.rb
@@ -14,4 +14,19 @@ class Application < ApplicationRecord
     PetApplication.where(pet_id: pet_id, application_id: self.id).first
   end
 
+  def approved? 
+    num_pets_on_app = pet_applications.select("COUNT(*) AS pet_count")[0].pet_count
+    num_pets_approved = pet_applications.select("COUNT(pet_applications.status) AS pet_app_status").where("pet_applications.status = 1")[0].pet_app_status
+    
+    num_pets_approved == num_pets_on_app
+  end
+
+  def rejected? 
+    num_pets_approved = pet_applications.select("COUNT(pet_applications.status) AS pet_app_status").where("pet_applications.status = 2")[0].pet_app_status > 0
+  end
+  
+  def pending? 
+    num_pets_pending = pet_applications.select("COUNT(pet_applications.status) AS pet_app_status").where("pet_applications.status = 1")[0].pet_app_status > 0
+  end
+
 end
