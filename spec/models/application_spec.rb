1edc88b3ebdbe89e3826ed0b9730501a502f9a0a
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-19 13:03:11 -0700

    Move all tests from spec model application.rb to correct file, application_spec.rb

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -27,10 +27,11 @@ RSpec.describe Application, type: :model do
 
     @pet_app_1 = PetApplication.create!(pet_id: @pet_1.id, application_id: @app_1.id, status: 1)
     @pet_app_2 = PetApplication.create!(pet_id: @pet_2.id, application_id: @app_2.id, status: 1)
+
   end
 
   describe "instance methods" do 
-    describe ".get_pet_app" do 
+    describe ".get_pet_app(pet_id)" do 
       it "returns a pet application instance given a pet id" do 
         expect(@app_1.get_pet_app(@pet_1.id)).to eq(@pet_app_1)
         expect(@app_2.get_pet_app(@pet_2.id)).to eq(@pet_app_2)
@@ -38,85 +39,88 @@ RSpec.describe Application, type: :model do
       end
     end
 
-    describe ".pet_search" do 
-      it "searches for pets in the system by name" do 
-        expect(@app_2.pet_search("Lobster")).to eq([@pet_2])
+    describe ".rejected?" do 
+      it "returns false if ANY of the pet applications on an application are rejected" do 
+        app_1 = Application.create!(
+          name: "Susan", 
+          street: "7654 Clover St", 
+          city: "Denver", 
+          state: "CO", 
+          zip: "80033", 
+          descr: "I love animals and am lonely.",
+          status: 0
+        )
+
+        app_2 = Application.create!(
+          name: "Bryan", 
+          street: "8888 Hampden Ave", 
+          city: "Aurora", 
+          state: "CO", 
+          zip: "80265", 
+          descr: "I am buff af.",
+          status: 0
+        )
+
+        shelter_1 = Shelter.create(name: "Aurora Shelter", city: "Aurora, CO", foster_program: false, rank: 9)
+        shelter_2 = Shelter.create(name: "RGV Animal Shelter", city: "Harlingen, TX", foster_program: false, rank: 5)
+
+        pet_1 = Pet.create!(adoptable: true, age: 1, breed: "sphynx", name: "Lucille Bald", shelter_id: shelter_1.id)
+        pet_2 = Pet.create!(adoptable: true, age: 3, breed: "doberman", name: "Lobster", shelter_id: shelter_1.id)
+        pet_3 = Pet.create!(adoptable: false, age: 2, breed: "saint bernard", name: "Beethoven", shelter_id: shelter_1.id)
+        pet_4 = Pet.create!(adoptable: true, age: 1, breed: "beagle", name: "Toaster", shelter_id: shelter_2.id)
+        pet_5 = Pet.create!(adoptable: true, age: 4, breed: "pitbull", name: "Hoser", shelter_id: shelter_2.id)
+
+        pet_app_1 = PetApplication.create!(pet_id: pet_1.id, application_id: app_1.id, status: 1)
+        pet_app_2 = PetApplication.create!(pet_id: pet_2.id, application_id: app_1.id, status: 1)
+        pet_app_3 = PetApplication.create!(pet_id: pet_4.id, application_id: app_1.id, status: 1)
+        pet_app_4 = PetApplication.create!(pet_id: pet_1.id, application_id: app_2.id, status: 2)
+        pet_app_5 = PetApplication.create!(pet_id: pet_5.id, application_id: app_2.id, status: 1)
+
+        expect(app_1.rejected?).to eq(false)
+        expect(app_2.rejected?).to eq(true)
       end
     end
 
-    it "will change status of application based on button clicked" do
-      button_1 = "Approve"
-      button_2 = "Reject"
-      @pet_app_1.update_pet_status(button_1)
-      expect(@pet_app_1.status).to eq("Approved")
-      @pet_app_2.update_pet_status(button_2)
-      expect(@pet_app_2.status).to eq("Rejected")
-     end
+    describe ".approved?" do 
+      it "returns true if an application has been approved (all pet applications are 'approved')" do 
+        app_1 = Application.create!(
+          name: "Susan", 
+          street: "7654 Clover St", 
+          city: "Denver", 
+          state: "CO", 
+          zip: "80033", 
+          descr: "I love animals and am lonely.",
+          status: 0
+        )
 
-    it "will return the pet application status for a pet" do 
-      shelter_1 = Shelter.create(name: "Aurora Shelter", city: "Aurora, CO", foster_program: false, rank: 9)
-      pet_1 = Pet.create!(adoptable: true, age: 1, breed: "sphynx", name: "Lucille Bald", shelter_id: shelter_1.id)
-      pet_2 = Pet.create!(adoptable: true, age: 3, breed: "doberman", name: "Lobster", shelter_id: shelter_1.id)
-      pet_3 = Pet.create!(adoptable: false, age: 2, breed: "saint bernard", name: "Beethoven", shelter_id: shelter_1.id)
-      app_1 = Application.create!(
-        name: "Susan", 
-        street: "7654 Clover St", 
-        city: "Denver", 
-        state: "CO", 
-        zip: "80033", 
-        descr: "I love animals and am lonely.",
-        status: 1
-      )
+        app_2 = Application.create!(
+          name: "Bryan", 
+          street: "8888 Hampden Ave", 
+          city: "Aurora", 
+          state: "CO", 
+          zip: "80265", 
+          descr: "I am buff af.",
+          status: 0
+        )
 
-      pet_app_1 = PetApplication.create!(pet_id: pet_1.id, application_id: app_1.id, status: 2)
-      pet_app_2 = PetApplication.create!(pet_id: pet_2.id, application_id: app_1.id, status: 2)
-      pet_app_3 = PetApplication.create!(pet_id: pet_3.id, application_id: app_1.id, status: 2)
+        shelter_1 = Shelter.create(name: "Aurora Shelter", city: "Aurora, CO", foster_program: false, rank: 9)
+        shelter_2 = Shelter.create(name: "RGV Animal Shelter", city: "Harlingen, TX", foster_program: false, rank: 5)
 
-      expect(app_1.all_pets_apps_appr?).to eq(true)
-    end
+        pet_1 = Pet.create!(adoptable: true, age: 1, breed: "sphynx", name: "Lucille Bald", shelter_id: shelter_1.id)
+        pet_2 = Pet.create!(adoptable: true, age: 3, breed: "doberman", name: "Lobster", shelter_id: shelter_1.id)
+        pet_3 = Pet.create!(adoptable: false, age: 2, breed: "saint bernard", name: "Beethoven", shelter_id: shelter_1.id)
+        pet_4 = Pet.create!(adoptable: true, age: 1, breed: "beagle", name: "Toaster", shelter_id: shelter_2.id)
+        pet_5 = Pet.create!(adoptable: true, age: 4, breed: "pitbull", name: "Hoser", shelter_id: shelter_2.id)
 
-    it "will return the pet application status for a pet" do 
-      shelter_1 = Shelter.create(name: "Aurora Shelter", city: "Aurora, CO", foster_program: false, rank: 9)
-      pet_1 = Pet.create!(adoptable: true, age: 1, breed: "sphynx", name: "Lucille Bald", shelter_id: shelter_1.id)
-      pet_2 = Pet.create!(adoptable: true, age: 3, breed: "doberman", name: "Lobster", shelter_id: shelter_1.id)
-      pet_3 = Pet.create!(adoptable: false, age: 2, breed: "saint bernard", name: "Beethoven", shelter_id: shelter_1.id)
-      app_1 = Application.create!(
-        name: "Susan", 
-        street: "7654 Clover St", 
-        city: "Denver", 
-        state: "CO", 
-        zip: "80033", 
-        descr: "I love animals and am lonely.",
-        status: 1
-      )
+        pet_app_1 = PetApplication.create!(pet_id: pet_1.id, application_id: app_1.id, status: 1)
+        pet_app_2 = PetApplication.create!(pet_id: pet_2.id, application_id: app_1.id, status: 1)
+        pet_app_3 = PetApplication.create!(pet_id: pet_4.id, application_id: app_1.id, status: 1)
+        pet_app_4 = PetApplication.create!(pet_id: pet_1.id, application_id: app_2.id, status: 2)
+        pet_app_5 = PetApplication.create!(pet_id: pet_5.id, application_id: app_2.id, status: 1)
 
-      pet_app_1 = PetApplication.create!(pet_id: pet_1.id, application_id: app_1.id, status: 2)
-      pet_app_2 = PetApplication.create!(pet_id: pet_2.id, application_id: app_1.id, status: 3)
-      pet_app_3 = PetApplication.create!(pet_id: pet_3.id, application_id: app_1.id, status: 2)
-
-      expect(app_1.all_pets_apps_appr?).to eq(false)
-    end
-
-    it "will return nil if there are only Approved and Pending pet applications on an application" do
-      shelter_1 = Shelter.create(name: "Aurora Shelter", city: "Aurora, CO", foster_program: false, rank: 9)
-      pet_1 = Pet.create!(adoptable: true, age: 1, breed: "sphynx", name: "Lucille Bald", shelter_id: shelter_1.id)
-      pet_2 = Pet.create!(adoptable: true, age: 3, breed: "doberman", name: "Lobster", shelter_id: shelter_1.id)
-      pet_3 = Pet.create!(adoptable: false, age: 2, breed: "saint bernard", name: "Beethoven", shelter_id: shelter_1.id)
-      app_1 = Application.create!(
-        name: "Susan", 
-        street: "7654 Clover St", 
-        city: "Denver", 
-        state: "CO", 
-        zip: "80033", 
-        descr: "I love animals and am lonely.",
-        status: 1
-      )
-
-      pet_app_1 = PetApplication.create!(pet_id: pet_1.id, application_id: app_1.id, status: 2)
-      pet_app_2 = PetApplication.create!(pet_id: pet_2.id, application_id: app_1.id, status: 2)
-      pet_app_3 = PetApplication.create!(pet_id: pet_3.id, application_id: app_1.id, status: 1)
-
-      expect(app_1.all_pets_apps_appr?).to eq(nil)
+        expect(app_1.approved?).to eq(true)
+        expect(app_2.approved?).to eq(false)
+      end
     end
   end
 end 
