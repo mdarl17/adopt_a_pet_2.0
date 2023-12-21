8ade20fa5bfb899a10b5ed708f37717aec27af70
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-21 14:23:24 -0700

    Remove before(:each) setup data as it was not necessary for any of the test setups

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -1,23 +1,6 @@
 require "rails_helper"
 
 RSpec.describe "the pets index" do 
-  before(:each) do 
-    @shelter = Shelter.create!(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
-    @pet_1 = Pet.create!(adoptable: true, age: 1, breed: "sphynx", name: "Rocky", shelter_id: @shelter.id)
-    @pet_2 = Pet.create!(adoptable: true, age: 3, breed: "doberman", name: "Bullwinkle", shelter_id: @shelter.id)
-    @pet_3 = Pet.create!(adoptable: false, age: 2, breed: "saint bernard", name: "Boris", shelter_id: @shelter.id)
-    @app_1 = Application.create!(name: "Susan", 
-      street: "7654 Clover St", 
-      city: "Denver", 
-      state: "CO", 
-      zip: "80033", 
-      descr: "I love animals and am lonely")
-   
-    @pet_1_app =PetApplication.create!(pet_id: @pet_1.id, application_id: @app_1.id)
-    @app_1.pets << @pet_2
-    @app_1.pets << @pet_3
-   end
-
   it "lists all the pets with their attributes" do
     shelter = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
     pet_1 = Pet.create(adoptable: true, age: 1, breed: "sphynx", name: "Lucille Bald", shelter_id: shelter.id)
@@ -38,8 +21,8 @@ RSpec.describe "the pets index" do
 
   it "only lists adoptable pets" do
     shelter = Shelter.create(name: "Aurora shelter", city: "Aurora, CO", foster_program: false, rank: 9)
-    # pet_1 = Pet.create(adoptable: true, age: 1, breed: "sphynx", name: "Lucille Bald", shelter_id: shelter.id)
-    # pet_2 = Pet.create(adoptable: true, age: 3, breed: "doberman", name: "Lobster", shelter_id: shelter.id)
+    pet_1 = Pet.create(adoptable: true, age: 1, breed: "sphynx", name: "Lucille Bald", shelter_id: shelter.id)
+    pet_2 = Pet.create(adoptable: true, age: 3, breed: "doberman", name: "Lobster", shelter_id: shelter.id)
     pet_3 = Pet.create(adoptable: false, age: 2, breed: "saint bernard", name: "Beethoven", shelter_id: shelter.id)
 
     visit "/pets"
