1c8ac3d8b4a24af2a742f3f01dbedc93f6c915a3
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-21 14:13:18 -0700

    Change adoptable status of @pet_3 (Beethoven) to 'true' to make the setup data create a more realistic environment from the start - where each application and the pet applications associated with them are all 'pending.'

--- db/seeds.rb
+++ db/seeds.rb
@@ -47,7 +47,7 @@ Shelter.destroy_all
 
     @pet_1 = Pet.create!(adoptable: true, age: 1, breed: "sphynx", name: "Lucille Bald", shelter_id: @shelter_1.id)
     @pet_2 = Pet.create!(adoptable: true, age: 3, breed: "doberman", name: "Lobster", shelter_id: @shelter_1.id)
-    @pet_3 = Pet.create!(adoptable: false, age: 2, breed: "saint bernard", name: "Beethoven", shelter_id: @shelter_2.id)
+    @pet_3 = Pet.create!(adoptable: true, age: 2, breed: "saint bernard", name: "Beethoven", shelter_id: @shelter_2.id)
     @pet_4 = Pet.create!(adoptable: true, age: 1, breed: "beagle", name: "Toaster", shelter_id: @shelter_3.id)
     @pet_5 = Pet.create!(adoptable: true, age: 4, breed: "pitbull", name: "Hoser", shelter_id: @shelter_3.id)
 
