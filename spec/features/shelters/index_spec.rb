9b5b1d0d24ee6b44cef91435d74731d3f29ed5b9
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-22 16:25:07 -0700

    Add more pets to make testing the 'sort by number of pets' link more robust.

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -7,8 +7,10 @@ RSpec.describe "the shelters index" do
     @shelter_3 = Shelter.create(name: "Fancy pets of Colorado", city: "Denver, CO", foster_program: true, rank: 10)
     @shelter_1.pets.create(name: "Mr. Pirate", breed: "tuxedo shorthair", age: 5, adoptable: true)
     @shelter_1.pets.create(name: "Clawdia", breed: "shorthair", age: 3, adoptable: true)
+    @shelter_1.pets.create(name: "Juneau", breed: "husky", age: 7, adoptable: true)
     @shelter_2.pets.create(name: "Lobster", breed: "pug", age: 2, adoptable: true)
     @shelter_3.pets.create(name: "Lucille Bald", breed: "sphynx", age: 8, adoptable: true)
+    @shelter_3.pets.create(name: "Hoser", breed: "bulldog", age: 4, adoptable: true)
   end
 
   it "lists all the shelter names" do
@@ -49,8 +51,8 @@ RSpec.describe "the shelters index" do
     click_link("Sort by number of pets")
 
     expect(page).to have_current_path("/shelters?sort=pet_count")
-    expect(@shelter_1.name).to appear_before(@shelter_2.name)
-    expect(@shelter_2.name).to appear_before(@shelter_3.name)
+    expect(@shelter_1.name).to appear_before(@shelter_3.name)
+    expect(@shelter_3.name).to appear_before(@shelter_2.name)
   end
 
   it "has a link to update each shelter" do
