39bb349be922f14b524f3864d93e87377c5434ae
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-23 16:47:46 -0700

    New pet application mode class method - #pending_pet_applications - that finds pets on multiple pending applications.  Will later set up an alert to warn user that pet is on a different app which could affect pet status (and app status if rejected) even if pet was initially approved on current application

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -4,4 +4,8 @@ class PetApplication < ApplicationRecord
 
   enum status: { "Pending": 0, "Approved": 1, "Rejected": 2 }
 
+  def self.pending_pet_applications(p_id) 
+    joins(:application).where("pet_applications.pet_id = ?", p_id).where("applications.status = 1").count > 1
+  end
+
 end
