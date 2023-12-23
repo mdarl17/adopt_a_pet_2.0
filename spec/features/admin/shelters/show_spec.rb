79f7b7f72a14849eeb9f060cc12bf313d21fe3e8
Matt Darlington <matthewsdarlington@gmail.com>
2023-12-22 20:03:13 -0700

    New feature tests ensuring #action_required Shelter instance method is present, properly displayed, and with the correct pet names

--- spec/features/admin/shelters/show_spec.rb
+++ spec/features/admin/shelters/show_spec.rb
@@ -23,6 +23,7 @@ RSpec.describe "Admin Shelter Show Page" do
     )
 
     @shelter_1 = Shelter.create(name: "Aurora Shelter", city: "Aurora, CO", foster_program: false, rank: 9, created_at: Date.today-3)
+    @shelter_2 = Shelter.create(name: "RGV Animal Shelter", city: "Harlingen, TX", foster_program: false, rank: 5)
 
     @pet_1 = Pet.create!(adoptable: true, age: 1, breed: "sphynx", name: "Lucille Bald", shelter_id: @shelter_1.id)
     @pet_2 = Pet.create!(adoptable: true, age: 3, breed: "doberman", name: "Lobster", shelter_id: @shelter_1.id)
@@ -30,9 +31,16 @@ RSpec.describe "Admin Shelter Show Page" do
     @pet_4 = Pet.create!(adoptable: true, age: 1, breed: "beagle", name: "Toaster", shelter_id: @shelter_1.id)
     @pet_5 = Pet.create!(adoptable: false, age: 4, breed: "pitbull", name: "Hoser", shelter_id: @shelter_1.id)
     @pet_6 = Pet.create!(adoptable: true, age: 4, breed: "husky", name: "Juneau", shelter_id: @shelter_1.id)
-    @pet_7 = Pet.create!(adoptable: true, age: 1, breed: "rotweiler/beagle", name: "Curtis", shelter_id: @shelter_1.id)
-    @pet_8 = Pet.create!(adoptable: true, age: 1, breed: "wolf mix", name: "Marley", shelter_id: @shelter_1.id)
-    @pet_9 = Pet.create!(adoptable: true, age: 2, breed: "australian sheep", name: "Chai", shelter_id: @shelter_1.id)
+    @pet_7 = Pet.create!(adoptable: true, age: 1, breed: "rotweiler/beagle", name: "Curtis", shelter_id: @shelter_2.id)
+    @pet_8 = Pet.create!(adoptable: true, age: 1, breed: "wolf mix", name: "Marley", shelter_id: @shelter_2.id)
+    @pet_9 = Pet.create!(adoptable: true, age: 2, breed: "australian sheep", name: "Chai", shelter_id: @shelter_2.id)
+
+    @pet_app_1 = PetApplication.create!(application_id: @app_1.id, pet_id: @pet_1.id, status: 0)
+    @pet_app_2 = PetApplication.create!(application_id: @app_1.id, pet_id: @pet_2.id, status: 1)
+    @pet_app_4 = PetApplication.create!(application_id: @app_1.id, pet_id: @pet_4.id, status: 0)
+    @pet_app_6 = PetApplication.create!(application_id: @app_1.id, pet_id: @pet_6.id, status: 1)
+    @pet_app_7 = PetApplication.create!(application_id: @app_2.id, pet_id: @pet_8.id, status: 2)
+    @pet_app_8 = PetApplication.create!(application_id: @app_2.id, pet_id: @pet_9.id, status: 0)
   end 
 
   it "shows the Shelter's name and address on the admin shelter show page" do
@@ -64,6 +72,21 @@ RSpec.describe "Admin Shelter Show Page" do
 
     within ".statistics" do 
       expect(page).to have_content("No. Pets Adopted: 2")
+    end
+  end
+
+  it "has a section titled 'Action Required' that displays a list of a shelter's pets that have a pending pet application (and, subsequently, 'Pending' status on main application)" do 
+    visit "/admin/shelters/#{@shelter_1.id}"
+
+    within ".action-required" do 
+      expect(page).to have_content("Lucille Bald")
+      expect(page).to have_content("Toaster")
+    end
+
+    visit "/admin/shelters/#{@shelter_2.id}"
+
+    within ".action-required" do 
+      expect(page).to have_content("Chai")
     end
   end
   
